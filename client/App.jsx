eb6b2ec2705f997f51b66ca185821beaf7b39de1
Elizabeth Hayt <enhayt@gmail.com>
2020-11-09 10:58:48 -0800

    Set up react-testing-library config

--- client/App.jsx
+++ client/App.jsx
@@ -1,5 +1,4 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
 import axios from 'axios';
 
 import Player from './Components/Player.jsx';
@@ -132,4 +131,4 @@ class App extends React.Component {
   }
 }
 
-ReactDOM.render(<App />, document.getElementById('app'));
+export default App;
