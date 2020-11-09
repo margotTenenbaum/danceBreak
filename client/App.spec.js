9d82df77c9da625e6a0b65847c1158b42ab287df
Elizabeth Hayt <enhayt@gmail.com>
2020-11-09 12:49:39 -0800

    Add basic react tests

--- client/App.spec.js
+++ client/App.spec.js
@@ -1,10 +1,33 @@
 import React from 'react';
-import { render } from '@testing-library/react';
+import { render, screen } from '@testing-library/react';
+import '@testing-library/jest-dom/extend-expect';
 
 import App from './App';
+import Player from './Components/Player.jsx';
 
 describe('App', () => {
   test('renders App component', () => {
     render(<App />);
+    //screen.debug();
+    expect(screen.getByText("Dance Break!")).toBeInTheDocument();
+    expect(screen.getByText("Find your favorite artist's most danceable track")).toBeInTheDocument();
+    expect(screen.getByText("Let's Dance")).toBeInTheDocument();
+    expect(screen.getByText("Add to playlist?")).toBeInTheDocument();
   });
+
+  test('renders Player component', () => {
+    render(<Player songID='4kbj5MwxO1bq9wjT5g9HaA' />);
+
+    expect(screen.getByTestId('player')).toHaveAttribute('src');
+  });
+
+  //test player with props
+
+  //test playlist with props
+
+
+
+
+
+
 });
--- client/Components/Player.jsx
+++ client/Components/Player.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 const Player = (props) => (
   <div>
-    <iframe src={"https://open.spotify.com/embed/track/" + props.songID} width="300" height="380" frameBorder="0" allowtransparency="true" allow="encrypted-media"></iframe>
+    <iframe data-testid='player' src={"https://open.spotify.com/embed/track/" + props.songID} width="300" height="380" frameBorder="0" allowtransparency="true" allow="encrypted-media"></iframe>
   </div>
 )
 
